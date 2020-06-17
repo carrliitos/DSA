4ce38a183e6b6fefe900359e30bcf641a3634884
carrliitos <salazar.ten80@gmail.com>
2020-06-17 13:55:42 -0500

    reviewed Hashing.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,15 +1,4 @@
 public class LinkedList {
-	class ListNode {
-		protected int value;
-		protected ListNode next;
-		/* Constructor that accepts a value as an argument and creates
-		* a ListNode object with that value
-		*/
-		public ListNode(int val) {
-			value = val;
-			next = null;
-		}
-	}
 
 	protected ListNode head, tail;
 	protected int size;
---
+++ ListNode.java
@@ -0,0 +1,11 @@
+public class ListNode {
+		protected int value;
+		protected ListNode next;
+		/* Constructor that accepts a value as an argument and creates
+		* a ListNode object with that value
+		*/
+		public ListNode(int val) {
+			value = val;
+			next = null;
+		}
+	}
