8e0ddfa5e81de481a5d2277d88d3e1bc26810322
carrliitos <salazar.ten80@gmail.com>
2020-06-17 18:15:38 -0500

    Added RotatedBinarySearch.java assignment

--- PA4/RotatedBinarySearch.java
+++ PA4/RotatedBinarySearch.java
@@ -1,9 +1,30 @@
 public class RotatedBinarySearch {
+
 	public static int search(int array[], int length, int key) {
-		
+		int maxInd = maxIndex(array, (array[length - 1]), array[0], (length - 1));
+
+		if(key == array[maxInd]) {
+			return maxInd;
+		}
+		if(key >= array[0]) {
+			return binarySearch(array, array[0], (maxInd - 1), key);
+		}else {
+			return binarySearch(array, (maxInd + 1), (length - 1), key);
+		}
 	}
 
-	private static int maxIndex(int array[], int lastValue, int left, int right) { // complete this function
+	private static int maxIndex(int array[], int lastValue, int left, int right) {
+		if(left == right) {
+			return left;
+		}
+		int mid = (left + right) / 2;
+		if(mid > (mid + 1)) {
+			return mid;
+		}else if(mid < lastValue) {
+			return maxIndex(array, lastValue, left, (mid - 1));
+		}else {
+			return maxIndex(array, lastValue, (mid + 1), right);
+		}
 	}
 
 	private static int binarySearch(int array[], int left, int right, int key) {
