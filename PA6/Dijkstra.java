4f7274f63c5a84fbf7d12d499dbcefb7303863a0
carrliitos <salazar.ten80@gmail.com>
2020-07-01 12:37:55 -0500

    Made some changes and deleted all comments in prep for final submission

--- PA6/Dijkstra.java
+++ PA6/Dijkstra.java
@@ -1,6 +1,6 @@
 import java.io.FileNotFoundException;
 
-public class Dijkstra extends DijkstraGraph {
+public class Dijkstra extends Graph {
 
 	public int distance[], parent[];
 	private boolean closed[];
@@ -13,58 +13,38 @@ public class Dijkstra extends DijkstraGraph {
 	}
 
 	public void executeDijkstra(int source) { 
-		// for (i = 0 to i < numVertices), do the following:
 		for(int i = 0; i < numVertices; i++) {
-			// set distance[i] = ∞ (INT MAX in C++ and Integer.MAX VALUE in Java)
 			distance[i] = Integer.MAX_VALUE;
-			// set parent[i] to −1
 			parent[i] = -1;
-			// set closed[i] to false
 			closed[i] = false;
 		}
-		// Set distance[source] to 0
+
 		distance[source] = 0;
-		// Create an integer variable numOpen and set it to 1
 		int numOpen = 1;
-		// As long as (numOpen > 0), do the following:
+
 		while(numOpen > 0) {
-			// Initialize a variable minDist to ∞ and another variable minVertex to −1. These will be used to expand the open vertex with the minimum distance label.
 			int minDist = Integer.MAX_VALUE;
 			int minVertex = -1;
-			// for (i = 0 to i < numVertices), do the following:
 			for(int i = 0; i < numVertices; i++) {
-				// if (closed[i] is false and distance[i] < minDist), then:
 				if((closed[i] == false) && (distance[i] < minDist)){
-					// set minDist to distance[i]
 					minDist = distance[i];
-					// set minVertex to i
 					minVertex = i;
 				}
 			}
-			// set closed[minVertex] to true
+
 			closed[minVertex] = true;
-			// decrement numOpen by one
 			numOpen--;
-			// for (i = 0 to i < outDegree[minVertex]), do the following:
+
 			for(int i = 0; i < outDegree[minVertex]; i++) {
-				// let adjEdge be ith adjacent edge of minVertex
 				Edge adjEdge = adjList.get(minVertex).get(i);
-				// let adjVertex be the destination of adjEdge
 				int adjVertex = adjEdge.dest;
-				// if (adjVertex is not closed), do the following:
 				if(!(closed[adjVertex])){
-					// set newDist to distance[minVertex] + adjEdge0s weight
 					int newDist = distance[minVertex] + adjEdge.weight;
-					// if distance[adjVertex] equals ∞, 
 					if(distance[adjVertex] == Integer.MAX_VALUE){
-						// then the vertex has not been visited; so, increment numOpen by 1
 						numOpen++;
 					}
-					// if (newDist < distance[adjVertex]), then
 					if(newDist < distance[adjVertex]){
-						// set distance[adjVertex] to newDist
 						distance[adjVertex] = newDist;
-						// set parent[adjVertex] to minVertex
 						parent[adjVertex] = minVertex;
 					}
 				}
--- PA6/Trie.java
+++ PA6/Trie.java
@@ -7,24 +7,16 @@ public class Trie {
 	}
 
 	void insert(String str) {
-		// Set a TrieNode temporary variable tmp to the root. (In C++, tmp has to be a pointer.)
 		TrieNode tmp = root;
-		// for (i = 0 to i < str.length()), do the following:
+
 		for(int i = 0; i < str.length(); i++) {
-			// Let c be the character at position i of str.
 			char c = str.charAt(i);
-			// Use the getChild method on tmp with c as argument to get the child of tmp, such that the corresponding edge is labeled by c.
-			// If the child is not null, set tmp to child, i.e., just move to the child.
 			if(tmp.getChild(c) != null) {
 				tmp = tmp.insertChild(c);
 			}else {
-				// Else, as long as i < str.length(), do the following:
 				while(i < str.length()) {
-					// Use the insertChild method on tmp with the character at position i of str as argument (to insert a new child of tmp).
 					tmp.insertChild(str.charAt(i));
-					// The insertChild method returns the child; set tmp to the child.
 					tmp = tmp.insertChild(c);
-					// Increment i by 1.
 					i++;
 				}
 			}
@@ -32,22 +24,16 @@ public class Trie {
 	}
 
 	boolean match(String str) {
-		// Set a TrieNode temporary variable tmp to the root. (In C++, tmp has to be a pointer.)
 		TrieNode tmp = root;
-		// for (i = 0 to i < str.length()), do the following:
+
 		for(int i = 0; i < str.length(); i++) {
-			// Let c be the character at position i of str.
 			char c = str.charAt(i);
-			// Use the getChild method on tmp with c as argument to get the child of tmp, such that the corresponding edge is labeled by c.
-			// If the child is null, then a mismatch has occurred; hence, return false.
 			if(tmp.getChild(c) == null) {
 				return false;
-			// Else, set tmp to the child.
 			}else {
 				tmp = tmp.insertChild(c);
 			}
 		}
-		// Once the for-loop is done, the entire string has matched. Hence, return true.
 		return true;
 	}
 }
