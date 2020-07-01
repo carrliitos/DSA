17ec9f68a96e5dc9a882da1de4c61de50505edc3
carrliitos <salazar.ten80@gmail.com>
2020-06-30 21:39:29 -0500

    Working on HeapSort() and topK()

---
+++ HeapApplications.java
@@ -0,0 +1,9 @@
+public class HeapApplications {
+
+	public static void heapSort(String array[], int arrayLen) throws Exception {
+		Heap heap = new Heap();
+	}
+
+	// public static String[] topK(String array[], int arrayLen, int k) { // complete this function
+	// }
+}
