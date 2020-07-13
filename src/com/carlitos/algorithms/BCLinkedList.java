b4cf8076b2eca8b162431a97ae95bec332b9bf3e
carrliitos <salazar.ten80@gmail.com>
2020-07-13 18:04:04 -0500

    update BCLinkedList.java

--- src/com/carlitos/algorithms/BCLinkedList.java
+++ src/com/carlitos/algorithms/BCLinkedList.java
@@ -1,16 +1,16 @@
 /** @author Benzon Carlitos Salazar
-	This is my correct implementation of a single-linked list
+ This is my correct implementation of a single-linked list
 */
 
 	/** Class to represent a linked list with a link from each node to the next node.
-		SingleLinkedList does not implement the List interface.
+	 SingleLinkedList does not implement the List interface.
 	*/
 public class BCLinkedList<E> {
 	/** We create an inner class of Node
-		A Node is the building block for a single-linked list
+	 A Node is the building block for a single-linked list
 
-		The keyword static in the class header indicates that the 
-		Node<E> class will not reference its outer class.
+	 The keyword static in the class header indicates that the 
+	 Node<E> class will not reference its outer class.
 	*/
 	private static class Node<E> {
 		// Create a reference to the data
@@ -19,7 +19,7 @@ public class BCLinkedList<E> {
 		private Node<E> next;
 		
 		/** Create a new node constructor with a null next field
-			@param dataItem - The data stored
+		 @param dataItem - The data stored
 		*/
 		private Node(E dataItem) {
 			data = dataItem;
@@ -27,8 +27,8 @@ public class BCLinkedList<E> {
 		}
 
 		/** Create a new node constructor that references another node
-			@param dataItem - The data stored
-			@param nodeRef - The node reference by the new node
+		 @param dataItem - The data stored
+		 @param nodeRef - The node reference by the new node
 		*/
 		private Node(E dataItem, Node<E> nodeRef) {
 			data = dataItem;
@@ -42,13 +42,27 @@ public class BCLinkedList<E> {
 	private int size = 0;
 
 	/** Adding an item to the front of the list
-		@param dataItem - The item to be added
+	 @param dataItem - The item to be added
 	*/
 	public void addFirst(E dataItem) {
 		// Set head to a new Node with dataItem as the data stored and 
 		// the old head as the node reference
 		head = new Node<>(dataItem, head);
 		// increment the size to account for the new size after adding
+		size++;
+	}
+
+	/** Adding an item after a specific node
+	 @param node - The node preceding the new item
+	 @param item - The item to insert
+	*/
+	private void addAfter(Node<E> node, E item) {
+		/* Point the preceding node's next to the new node and initialize
+		 it to a new node with item as the data and node.next (preceding node's
+		 next) as the new node's next
+		*/
+		node.next = new Node<>(item, node.next);
+		// Increment the size
 		size++;
 	}
 }
